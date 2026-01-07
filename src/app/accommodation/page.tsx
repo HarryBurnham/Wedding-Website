047e4acee10d0e9df1b7279bd98b6fdcb6374805
Harry Burnham <75851367+HarryBurnham@users.noreply.github.com>
2026-01-07 16:39:57 +0000

    Remoce 1 div

--- src/app/accommodation/page.tsx
+++ src/app/accommodation/page.tsx
@@ -125,7 +125,6 @@ export default function TravelAccommodation() {
             </div>
             </div>
           </div>
-        </div>
       </section>
 
       {/* Taxis & Local Transport */}
